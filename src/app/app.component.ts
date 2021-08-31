aa589be11721326c0216a8aa7612bb7fc625fe62
Frida <fridaahlstrom@outlook.com>
2021-08-31 17:56:52 +0100

    Editor created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rt-c-d-editor';
+  title = 'real time collaborative document editor';
+
+  constructor() { }
+  content:string = ``;
+
+  getContent(event : any) {
+    this.content = event;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { EditorComponent } from './components/editor/editor.component';
+
+import { QuillModule } from 'ngx-quill'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ToolbarComponent,
+    ButtonComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+
+    QuillModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
